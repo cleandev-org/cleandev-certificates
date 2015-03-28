af51f24f4f808008c42679a7055a2bc2e1cf5c45
Matheus Oliveira <oliveira.matheusde@gmail.com>
2015-03-28 16:27:51 -0300

    Certified

--- cleandevcertificates/settings.py
+++ cleandevcertificates/settings.py
@@ -18,7 +18,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
 # SITE_URL = 'http://127.0.0.1:8000'
-SITE_URL = 'http://cleandevcertificates.herokuapp.com'
+# SITE_URL = 'http://cleandevcertificates.herokuapp.com'
+SITE_URL = 'http://certificados.cleandev.org'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
