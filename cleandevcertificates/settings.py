b50563ba4abe72a48cd4f2e317df8c78ca631b6d
Matheus Oliveira <oliveira.matheusde@gmail.com>
2015-04-14 13:54:39 -0300

    add Django Flat Admin Theme

--- cleandevcertificates/settings.py
+++ cleandevcertificates/settings.py
@@ -39,8 +39,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
-    'django_admin_bootstrapped',
-
+    'flat',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
